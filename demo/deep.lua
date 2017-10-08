0c18ee850b9e38d92bbffb686c35e4162e1a9451
Nika Otiashvili <nikaoto@gmail.com>
2017-10-08 23:51:00 +0400

    optimize enqueuing

--- demo/deep.lua
+++ demo/deep.lua
@@ -5,15 +5,23 @@ local drawQueue = {}
 local defaults = {z = 0, r = 0, sx = 1, sy = 1, ox = 0, oy = 0, kx = 0, ky = 0}
 
 -- Rereate drawQueue if needed
-function deep:renewQueue()
+local function renewQueue()
 	if (drawQueue == nil) then
 		drawQueue = {}
 	end
 end
 
+local function enqueue(t, z)
+	if (drawQueue[z] == nil) then
+		drawQueue[z] = {t}
+	else
+		table.insert(drawQueue[z], t)
+	end
+end
+
 -- Queues up an object to draw according to its Z axis
 function deep:queue(drawable, x, y, z, r, sx, sy, ox, oy, kx, ky)
-	self.renewQueue()
+	renewQueue()
 
 	z = z or defaults.z
 	r = r or defaults.r
@@ -21,37 +29,24 @@ function deep:queue(drawable, x, y, z, r, sx, sy, ox, oy, kx, ky)
 	ox = ox or defaults.ox; oy = oy or defaults.oy
 	kx = kx or defaults.ks; ky = ky or defaults.ky
 
---[[	if (drawQueue[z] == nil) then
-		drawQueue[z] = {{drawable, x, y, r, sx, sy, ox, oy, kx, ky}}
-	else
-		table.insert(drawQueue[z], {drawable, x, y, r, sx, sy, ox, oy, kx, ky})
-	end--]]
 	local temp = {drawable, x, y, r, sx, sy, ox, oy, kx, ky}
 	setmetatable(temp, {__call = function () 
-		love.graphics.draw(temp[1], temp[2], temp[3], temp[4], temp[5],
+			love.graphics.draw(temp[1], temp[2], temp[3], temp[4], temp[5],
 				temp[6], temp[7], temp[8], temp[9], temp[10], temp[11]) 
 		end})
 
-	if (drawQueue[z] == nil) then
-		drawQueue[z] = {temp}
-	else
-		table.insert(drawQueue[z], temp)
-	end
+	enqueue(temp, z)
 end
 
 function deep:rectangle(mode, x, y, z, width, height)
-	self.renewQueue()
+	renewQueue()
 
 	local temp = {mode, x, y, width, height}
 	setmetatable(temp, {__call = function() 
-		love.graphics.rectangle(temp[1], temp[2], temp[3], temp[4],temp[5]) 
+			love.graphics.rectangle(temp[1], temp[2], temp[3], temp[4],temp[5]) 
 		end})
 
-	if (drawQueue[z] == nil) then
-		drawQueue[z] = {temp}
-	else
-		table.insert(drawQueue[z], temp)
-	end
+	enqueue(temp, z)
 end
 
 -- Draws every queued object in order of their Z axii

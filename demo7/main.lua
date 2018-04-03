6c4989e2393136d9a7af3d5e3073f8572de220eb
Nikaoto <nikaoto@gmail.com>
2018-04-03 23:22:32 +0400

    update demo7

--- demo7/main.lua
+++ demo7/main.lua
@@ -4,28 +4,28 @@ local deep = require "../deep"
 local current_z = 1
 
 -- Draws a rectangle at passed y coordinate
-local draw_rectangle = function(y)
+local draw_line = function(y)
+  love.graphics.setColor(255, 255, 255) -- Reset color
   love.graphics.rectangle("fill", 200, y, 300, 10)
 end
 
 function love.draw()
-  deep.queue(2, draw_rectangle, 60)
-  deep.queue(3, draw_rectangle, 80)
-  deep.queue(4, draw_rectangle, 100)
+  deep.queue(2, draw_line, 60)
+  deep.queue(3, draw_line, 80)
+  deep.queue(4, draw_line, 100)
 
 
   -- Red square, which can move through z axis
   deep.queue(current_z, function()
     love.graphics.setColor(255, 0, 0) -- Set color to red
     love.graphics.rectangle("fill", 300, 40, 80, 80)
-    love.graphics.setColor(255, 255, 255) -- Reset color
   end)
 
   -- Draw everything in the queue
   deep.execute()
 end
 
--- Increases/decreases player z on key press
+-- Increases/decreases red square z on key press
 function love.keypressed(key)
   if key == "up" then
     current_z = current_z + 1

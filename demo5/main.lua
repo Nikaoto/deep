0c50fe6caf550326a04a9f3d1c04ef9280b1ed64
Nikaoto <nikaoto@gmail.com>
2018-04-04 01:05:59 +0400

    update demo5 for love 11.0; adapt to new api

--- demo5/main.lua
+++ demo5/main.lua
@@ -5,7 +5,7 @@ local deep = require "../deep"
 --
 
 function love.load()
-	Y_MOVE_MOD = 0.85 -- For the illusion of moving forward or backward
+	Y_MOVE_MOD = 0.85 -- For the illusion of moving forward or backward, slows vertical movement
 
 	player = {
 		speed = 350,
@@ -48,30 +48,30 @@ end
 function love.draw()
 	-- (color, mode, x, y, z, width, height)
 	deep.queue(150, function()
-		love.graphics.setColor(255, 255, 0)
+		love.graphics.setColor(1, 1, 0)
 		love.graphics.rectangle("fill", 0, 100, 500, 50)
 	end)
 
 	deep.queue(190, function()
-		love.graphics.setColor(0, 255, 0)
+		love.graphics.setColor(0, 1, 0)
 		love.graphics.rectangle("fill", 300, 140, 500, 50)
 	end)
 
 	-- Blue
 	deep.queue(230, function()
-		love.graphics.setColor(0, 0, 255)
+		love.graphics.setColor(0, 0, 1)
 		love.graphics.rectangle("fill", 0, 180, 500, 50)
 	end)
 
-	-- Orange
+	-- Cyan
 	deep.queue(270, function()
-		love.graphics.setColor(255, 128, 0)
+		love.graphics.setColor(0, 1, 1)
 		love.graphics.rectangle("fill", 300, 220, 500, 50)
 	end)
 
-	-- Queue the player
+	-- Queue the player (red cube)
 	deep.queue(player.z, function()
-		love.graphics.setColor(255, 0, 0)
+		love.graphics.setColor(1, 0, 0)
 		love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
 	end)
 	
